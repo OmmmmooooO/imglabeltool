494be45a2a44d5d549cb6016d9dcfdf0436a8720
OmmmmooooO <ryk_liu@compal.com>
2019-02-18 21:50:12 +0800

    finished cut.py customized size TBD

--- cut.py
+++ cut.py
@@ -15,21 +15,26 @@ if Path(self.json_file).exists():
 def main():
     json_file    = 'dataset/xray/coordinates.json'
     dataset_file = 'dataset/xray/'
+    cropped_file = 'cropped_imgs/'
+    
     if Path(json_file).exists():
         with open(json_file, 'r') as f:
             json_data = json.load(f)
-        #print(json.dumps(json_data, indent=4, sort_keys=True))
+        print(json.dumps(json_data, indent=4, sort_keys=True))
 
-        for i in range(0, len(json_data)):
-            print(json_data[i]['patientID'])
+        for i in range(0, len(json_data)):           
             ori_img = cv2.imread(dataset_file + json_data[i]['patientID'] + '.jpg')
             img     = ori_img.copy()
 
             ###e.g. CROP_IMG = IMG[y:y+h, x:x+w]
             img_L   = img[json_data[i]['left_y']:json_data[i]['left_y']+json_data[i]['left_height'], json_data[i]['left_x']:json_data[i]['left_x']+json_data[i]['left_width']]
-            img_R   = img[]
+            img_R   = img[json_data[i]['right_y']:json_data[i]['right_y']+json_data[i]['right_height'], json_data[i]['right_x']:json_data[i]['right_x']+json_data[i]['right_width']]
             
-            #cv2.imshow("cropped", crop_img)
+            filename_L = cropped_file + json_data[i]['patientID'] + '_L.jpg'
+            filename_R = cropped_file + json_data[i]['patientID'] + '_R.jpg'
+
+            cv2.imwrite(filename_L, img_L)
+            cv2.imwrite(filename_R, img_R)
 
 if __name__ == '__main__':
     main()
